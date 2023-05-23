f6bb136d4f11667741fcef17e3c441fdc8055e6c
dogunfx <dogunfx@gmail.com>
2023-05-23 10:28:57 +0100

    completed the rough work

---
+++ queries.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE contact (
+--     contact_id int NOT NULL,
+--     contact_name varchar(255)
+-- );
+
+
+-- ALTER TABLE contact 
+--     ADD COLUMN contact_email NOT NULL;
+
+
+ALTER TABLE contact
+    DROP COLUMN contact_email;
+
+-- CREATE DATABASE testDB;
+
+-- DROP TABLE contact;
